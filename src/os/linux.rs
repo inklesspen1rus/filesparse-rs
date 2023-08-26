f5a9cfb68a2e04d4a3aa01c4218cdc2ebfd678d8
inklesspen1rus <inklesspen@yandex.ru>
2023-08-27 00:56:55 +0300

    Fix linux implementation

--- src/os/linux.rs
+++ src/os/linux.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, os::fd::AsRawFd, io::Error};
 
-use libc::{fallocate64, FALLOC_FL_PUNCH_HOLE};
+use libc::{fallocate64, FALLOC_FL_PUNCH_HOLE, FALLOC_FL_KEEP_SIZE};
 
 use crate::FileSparse;
 
@@ -11,11 +11,12 @@ impl FileSparse for File {
 
     fn deallocate_region(&self, region: (u64, u64)) -> Result<(), std::io::Error> {
         assert!(region.1 > region.0 && region.1 < i64::MAX as _);
-        let ret = unsafe { fallocate64(self.as_raw_fd(), FALLOC_FL_PUNCH_HOLE, (region.0) as _, (region.1 - region.0) as _) };
+        let ret = unsafe { fallocate64(self.as_raw_fd(), FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, (region.0) as _, (region.1 - region.0) as _) };
 
         match ret {
             0 => Ok(()),
-            x => Err(Error::from_raw_os_error(x))
+            -1 => Err(Error::last_os_error()),
+            _ => unreachable!()
         }
     }
 }
